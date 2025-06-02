27ad28db57fe5669aef2a87caf62a85c7be42251
zoli0829 <zoli0829@gmail.com>
2025-06-02 10:22:26 +0100

    finished day 96

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct HighScoreView: View {
+    @Environment(Player.self) var player
+    
+    var body: some View {
+        @Bindable var player = player
+        
+        Stepper("High score: \(player.highScore)", value: $player.highScore)
+    }
+}
+
 struct ContentView: View {
+    @State private var player = Player()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome")
+            HighScoreView()
         }
-        .padding()
+        .environment(player)
     }
 }
 
